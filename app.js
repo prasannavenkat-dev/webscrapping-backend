43d32128a0faed32e4f783d15822a6ab35e4e335
unknown <prasannavenkat1998@yahoo.com>
2021-05-02 15:43:34 +0530

    fourth commit

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 
 
 
